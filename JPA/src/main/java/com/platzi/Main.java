3108896427d00def8acc4a1f7cccbdd34c43b3b0
Andres-Bermudez <andres201900@outlook.com>
2025-01-14 18:11:32 -0500

    Adding CRUD methods

--- JPA/src/main/java/com/platzi/Main.java
+++ JPA/src/main/java/com/platzi/Main.java
@@ -9,10 +9,47 @@ public class Main {
     public static void main(String[] args) {
         EntityManager entityManager= UtilEntityManagerFactory.getEntityManager();
 
+        // Obtener lista de empleados.
         List<Employee> employeeList =
                 entityManager.createQuery("SELECT * FROM employees", Employee.class)
                              .getResultList();
 
         employeeList.forEach(System.out::println);
+
+        // Buscar un empleado por su Id.
+        Employee employee = entityManager.find(Employee.class, 1);
+        System.out.println("Empleado encontrado: " + employee);
+
+        // Crear un nuevo empleado.
+        Employee newEmployee = new Employee(
+                null,
+                "Laura",
+                "Meneses",
+                "Urrutia",
+                "laura.meneses@email.com",
+                145000.0
+        );
+
+        entityManager.getTransaction(),begin(); // Iniciar la transaccion.
+        entityManager.persist(newEmployee);
+        entityManager.getTransaction().commit(); // Hacer commit de la transaccion.
+        System.out.println("Se ha creado un nuevo empleado.");
+
+        // Actualizar un registro.
+        Employee updateEmployee = entityManager.find(Employee.class, 1);
+        updateEmployee.setEmail("newEmail@email.com");
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(updateEmployee);
+        entityManager.getTransaction().commit();
+
+        // Eliminar un registro.
+        Employee deleteEmployee = entityManager.find(Employee.class, 1);
+
+        entityManager.getTransaction().begin();
+        entityManager.remove(deleteEmployee);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
     }
 }
